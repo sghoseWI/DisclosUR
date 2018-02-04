a4076afc51c41f993b2876594749af1b797de00a
Nick Tallant <ndtallant@uchicago.edu>
2018-02-04 16:15:16 -0600

    Created url from input, need to figure out what to do with the key

--- open_states/get_legislator.py
+++
@@ -1,8 +0,0 @@
-# Returns Legislators and Metadata from a latitiude and longitude
-
-import pyopenstates
-
-#api key stored locally for now - probably shouldn't be public.
-pyopenstates.set_api_key(api_jey)
-
-legislators = pyopenstates.locate_legislators(lat,lon)
--- open_states/open_states.py
+++ open_states/open_states.py
@@ -3,8 +3,19 @@
 # Returns Legislators and Metadata from a latitiude and longitude
 
 import pyopenstates
+import urllib3
+import json
 
 #api key stored locally for now - probably shouldn't be public.
-pyopenstates.set_api_key(api_jey)
+pyopenstates.set_api_key(ok_jey)
+
+gmaps_key = ""
+
+location_string = "+".join(address.split())
+url = "https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}".format(location_string, gmaps_key)
+#example https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+
+# Extract lat and lon from JSON here
 
 legislators = pyopenstates.locate_legislators(lat,lon)
+
