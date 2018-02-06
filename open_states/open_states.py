8e63d13f552f3fc396f8912af41725136a793da6
Nick Tallant <ndtallant@uchicago.edu>
2018-02-06 10:06:30 -0600

    Have correct output using requests module

--- open_states/open_states.py
+++ open_states/open_states.py
@@ -3,30 +3,26 @@
 # Returns Legislators and Metadata from a latitiude and longitude
 
 import pyopenstates
-import urllib3
+import requests 
 import json
 
 #api key stored locally for now - probably shouldn't be public.
-#pyopenstates.set_api_key(ok_jey)
+#pyopenstates.set_api_key(os_key)
 
 gmaps_key = ""
 
 def get_google_data(gmaps_key, address):
-	'''
-	Returns location data in JSON
-	'''
-	location_string = "+".join(address.split())
-	myurl = "https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}".format(location_string, gmaps_key)
-	#example https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+    '''
+    Returns location data in JSON
+    '''
+    location_string = "+".join(address.split())
+    myurl = "https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}".format(location_string, gmaps_key)
+    #example https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
 
-	# Extract lat and lon from JSON here
-	pm = urllib3.PoolManager()
+    # Extract lat and lon from JSON here
 
-	google_data = pm.urlopen(url=myurl, method="GET")
-	hm = google_data.read()
-	
-	#rv = json.loads(hm.decode())
-	return type(hm)
+    r = requests.get(myurl)
+    return r.json() 
 
 #legislators = pyopenstates.locate_legislators(lat,lon)
 
