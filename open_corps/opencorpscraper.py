696df6858ccfceaf8640522da43abd434efecd2e
SRGallicchio <35676395+SRGallicchio@users.noreply.github.com>
2018-02-21 23:55:23 -0600

    Bulk Scraper

--- open_corps/opencorpscraper.py
+++ open_corps/opencorpscraper.py
@@ -2,60 +2,79 @@ import requests
 import json
 import urllib
 import pandas as pd
+from dotenv import get_key
 
-test_leg_one = {"lawmaker_id":23076, "lawmaker":"ROSA, DENNIS", "state":"MA",
-"interests":["AMERICAN AUTO BODY AND REPAIR INC", "EAGLE REALTY TRUST"]}
-test_leg_two = {"lawmaker_id":6681989, "lawmaker":"YORK, JILL", "state":"KY",
-"interests":["PRINTWORKS UNLIMITED INC."]}
-test_leg_three = {"lawmaker_id":121340, "lawmaker":"PIGMAN, EDWIN", "state":"FL",
-"interests":["CALADIUM ER SVCS PARTNERSHIP LLC", "CARLTON ER SVCS PARTNERSHIP LLC",
-"CITY CIRCLE ER SVCS PARTNERSHIP, LLC", "DOCTORS MEMORIAL HOSPITAL", "HARDEE CO.",
-]}
+#open_corp_key = get_key.find_dotenv(), "OPEN_CORP_KEY")
 
-def corp_scraper(cpi_file, row_index, num_rows):
+def corp_scraper(cpi_file):
+    leg_dicts = get_leg_dicts(cpi_file, row_index_start, row_index_stop, False)
+    for key in leg_dicts:
+        leg = leg_dicts[key]
+        for entry in leg["employer_business_interest"]:
+            for interest, open_corps in entry.items():
+                    open_corps.extend(get_entity_info(leg))
+
+def get_leg_dicts(cpi_file, row_index_start, row_index_stop, partial=False):
     '''Takes the CPI csv file and scrapes the OpenCorps API. Builds a dictionary
     of each unique legislator, where the key-value pairs are the information from
-    the CPI csv file. Use the parameters to configure the portion of
+    the CPI csv file. The employer / business interest field becomes a list of
+    dictionaries, with each dictionary key corresponding to an employer or
+    business interest; the dictionary's values will ultimately be a list of
+    OpenCorps dictionaries corresponding to companies potentially matching that
+    employer / business interest.
+
+    Use the parameters to configure the portion of
     the dataset we want to search, to manage number of API calls. '''
 
-    df = pd.read_csv(cpi_file, encoding = "ISO-8859-1")
-    for row in df.iterrows():
-        # create a dictionary for each lawmaker
-        #leg_dict = {df.lawmaker:{"state":df.state,
-        leg_dict["open_corps"] = get_entity_info(leg_dict)
+    df = pd.read_csv(cpi_file, index_col = "lawmaker_id", encoding = "ISO-8859-1")
+    if partial:
+        df = df.iloc[row_index_start:row_index_stop]
+    df2 = df.to_dict("index")
+    for key in df2:
+        df2[key]["employer_business_interest"] = []
+        #df2[key]["employer_business_interest"] = [{df2[key]["employer_business_interest"]:[]}]
+        df2[key]["industry"] = [df2[key]["industry"]]
+    for index, row in df.iterrows():
+        if {row["employer_business_interest"]} not in df2[index]["employer_business_interest"]:
+            df2[index]["employer_business_interest"].append({row["employer_business_interest"]:[]})
+        if row["industry"] not in df2[index]["industry"]:
+            df2[index]["industry"].append(row["industry"])
+    return df2
 
 def get_entity_info(leg_dict):
     ''' Takes a dictionary consisting of a legislator, their state, and their
     listed business / employer interests, and scrapes the OpenCorp API for
-    entities matching their business / employer interests'''
+    entities matching their business / employer interests. Returns a list of
+    OpenCorp results dictionaries for entities that may match the employer / business interest. '''
     leg_juris_code = "us_" + leg_dict["state"].lower()
     entity_numbers = []
     entity_list = []
     # for each business / employer interest listed by a legislator, call the
     # OpenCorp search API, filtering out results whose incorporation post-dates
     # the CPI dataset
-    for interest in leg_dict["interests"]:
-        print("Searching OpenCorp API for: ", interest)
-        interest = interest.replace(" ", "+")
-        incorp_filter = "&incorporation\date=:2016-01-01"
-        search_url = "https://api.opencorporates.com/v0.4/companies/search?q=" + interest + incorp_filter
-        print(search_url)
-        response = requests.get(search_url)
-        if response.status_code == 200:
-            result = json.loads(response.content)
-        else:
-            print("No Response") # test code if we're out of calls
-        # collect all company numbers that may match the business / employer interest
-        for entity in result["results"]["companies"]:
-            print("Considering ", entity["company"]["name"])
-            # if there are a lot of results, filter on the state; might want to filter
-            # on status too
-            if len(result["results"]["companies"]) > 5 and entity["company"]["jurisdiction_code"] == leg_juris_code:
-                print("Passing on", entity["company"]["name"], "because its jurisdiction code is ", entity["company"]["jurisdiction_code"], "not ", leg_juris_code)
-                pass
+    for i in leg_dict["employer_business_interest"]:
+        for entry in i:
+            print("Searching OpenCorp API for: ", entry)
+            entry = entry.replace(" ", "+")
+            incorp_filter = "&incorporation\date=:2016-01-01"
+            search_url = "https://api.opencorporates.com/v0.4/companies/search?q=" + entry + incorp_filter
+            print(search_url)
+            response = requests.get(search_url)
+            if response.status_code == 200:
+                result = json.loads(response.content)
             else:
-                num = entity["company"]["jurisdiction_code"] + "/" + entity["company"]["company_number"]
-                entity_numbers.append(num)
+                print("No Response") # test code if we're out of calls
+            # collect all company numbers that may match the business / employer interest
+            for entity in result["results"]["companies"]:
+                print("Considering ", entity["company"]["name"])
+                # if there are a lot of results, filter on the state; might want to filter
+                # on status too
+                if len(result["results"]["companies"]) > 5 and entity["company"]["jurisdiction_code"] != leg_juris_code:
+                    print("Passing on", entity["company"]["name"], "because its jurisdiction code is ", entity["company"]["jurisdiction_code"], "not ", leg_juris_code)
+                    pass
+                else:
+                    num = entity["company"]["jurisdiction_code"] + "/" + entity["company"]["company_number"]
+                    entity_numbers.append(num)
     # for each company number, call the OpenCorp API and collect the info dict
     # that corresponds to that number
     for entity in entity_numbers:
