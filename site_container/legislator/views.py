d742b380528cccf8d707f6c86dbeaae23ae75e19
Nick Tallant <ndtallant@uchicago.edu>
2018-02-24 15:11:39 -0600

    Implementing a basic form for address input

--- site_container/legislator/views.py
+++ site_container/legislator/views.py
@@ -3,6 +3,9 @@ views file for legislator  app
 '''
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Lawmaker
+from forms import LawmakerForm
+from django.http import HttpResponseRedirect
 
 def index(request):
     return HttpResponse("This is the legislator application index")
