237414b4281af2de34a4387c6d73837abfcf9adb
Nick Tallant <ndtallant@uchicago.edu>
2018-02-25 23:23:58 -0600

    Fixed typo

--- site_container/legislator/admin.py
+++ site_container/legislator/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Industry, Party, Corps, State, Lawmaker 
 
-admin.site.register(Industy)
+admin.site.register(Industry)
 admin.site.register(Party)
 admin.site.register(Corps)
 admin.site.register(State)
