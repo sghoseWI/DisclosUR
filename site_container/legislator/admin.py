cc20e48ad7142e5ae383572ce641a9b93daa2f0c
sghose <saptarshi@uchicago.edu>
2018-03-01 20:11:53 -0600

    removing broken site

--- site_container/legislator/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Industry, Party, Corps, State, Lawmaker 
-
-admin.site.register(Industry)
-admin.site.register(Party)
-admin.site.register(Corps)
-admin.site.register(State)
-admin.site.register(Lawmaker)
