420c2e7e3f9b27811ca562a3a3223fda126dd069
Nick Tallant <ndtallant@uchicago.edu>
2018-03-04 15:34:38 -0600

    Can now populate the db from script

---
+++ site_container/all_cpi.py
@@ -0,0 +1,69 @@
+'''
+Meant to populate the main database with all legislators from the
+2015 CPI dataset. This will return a list of tuples, with each tuple
+being:
+
+    lawmaker, state, district, body, corp, industry
+'''
+import sqlite3 
+
+def get_cpi_tuples():
+    '''
+    Queries just about everything from cpi.db and
+    returns a list of tuples. Should be called in views.py
+    '''
+    query = '''
+            SELECT lawmaker,
+                   state,
+                   district,
+                   body,
+                   employer_business_interest, 
+                   industry
+            FROM cpi
+            '''
+    db = sqlite3.connect('cpi.db')
+    db.text_factory = bytes 
+    c = db.cursor()
+    r = c.execute(query)
+    return r.fetchall()
+
+def clean_record(record):
+    '''
+    Takes in a tuple of byte strings and returns
+    a tuple with correct None values in unicode.
+    This was necessary becasue some corps were not
+    able to query in unicode from the db.
+    '''
+    last_first = record[0].decode('utf-8').split(',')
+    lawmaker = last_first[1][1:] + ' ' + last_first[0]
+    state = record[1].decode('utf-8')
+    district = record[2].decode('utf-8')
+    body = record[3].decode('utf-8')
+    try:
+        corp = record[4].decode('utf-8')
+       # if corp == 'N/A':
+        #    corp = None
+    except UnicodeDecodeError:
+        corp = 'unicode error'
+    industry = record[5].decode('utf-8')
+    return lawmaker, state, district, body, corp, industry
+
+
+# test code, please ignore
+'''
+import os, sys
+
+proj_path = "/path/to/my/project/"
+# This is so Django knows where to find stuff.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+sys.path.append(proj_path)
+
+# This is so my local_settings.py gets loaded.
+os.chdir(proj_path)
+
+# This is so models get loaded.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+<your script goes here>
+'''
---
+++ site_container/connect.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import django
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "disc_site.settings")
+django.setup()
+from legislator.models import * #be specific later
+
+import all_cpi
+
+L = all_cpi.get_cpi_tuples()
+
+for record in L:
+    lawmaker, state, district, body, corp_name, industry = all_cpi.clean_record(record)
+
+    indu_ob = Industry(name=industry)
+    indu_ob.save()
+    state_ob = State(name=state, abbr=state)
+    state_ob.save()
+    corp_ob, _ = Corps.objects.get_or_create(name=corp_name, industry=indu_ob, defaults=None)
+    corp_ob.save()
+    lm_ob = Lawmaker(name = lawmaker, district=district, body=body,
+                    state=state_ob, cpi_15=True)
+    lm_ob.save()
+    lm_ob.corps.add(corp_ob)
+    lm_ob.save()
+    print('saving', lawmaker)
+
+    #model.objects. get or create?
