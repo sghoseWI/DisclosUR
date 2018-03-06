706c16461efbe470fb979208a749cca596c0f0f3
Nick Tallant <ndtallant@uchicago.edu>
2018-03-06 04:43:21 -0600

    Populate script can now take in more than one df - ie takes both sets of cpi, now more pythonic

--- site_container/populate_from_cpi.py
+++ site_container/populate_from_cpi.py
@@ -8,29 +8,52 @@ import pandas as pd
 
 cpi_path = '../data_wrangling/CPI/'
 standard = 'cpi_cleaned.csv'
-std_df = pd.read_csv(cpi_path+standard,
+std_df = pd.read_csv(cpi_path + standard,
+                      index_col="lawmaker_id",
+                      encoding="latin1")
+non_std = 'cpi_nonstd_cleaned.csv'
+non_std_df = pd.read_csv(cpi_path + non_std,
                       index_col="lawmaker_id",
                       encoding="latin1")
 
-for index, row in std_df.iterrows():
-#    check database to see if legislator id exists
+def create_fi(row, lawmaker): 
+    '''
+    Create FI object and associate it to the lawmaker
+    '''
+    fi_ob = FinancialInterest(name=row["employer_business_interest"],
+                              industry=row["industry"],
+                              lawmaker=lawmaker)
+    fi_ob.save()
+ 
+
+def create_lm(index, row):
+    '''
+    Checks to see if LM already exists, creates one if it does not.
+    Creates FI object tied to LM either way.
+    '''
     if not Lawmaker.objects.filter(pk=index).exists():
-        # create the lawmaker
-        lawmaker = Lawmaker.objects.create(name = row["lawmaker"],
-            state = row["state"],
-            body = row["body"],
-            lawmaker_id = index,
-            district = row["district"],
-            disclosure_url = row["disclosure_report"],
-            party = row["party"],
-            cpi_2015 = True)
+        lawmaker = Lawmaker(name=row["lawmaker"],
+                            state=row["state"],
+                            body=row["body"],
+                            lawmaker_id=index,
+                            district=row["district"],
+                            disclosure_url=row["disclosure_report"],
+                            party=row["party"],
+                            cpi_2015=True)
         lawmaker.save()
         print('saving', lawmaker)
 
     lawmaker = Lawmaker.objects.get(pk=index)
-    
-    # create FI object  and associate it to the lawmaker
-    fi_entity = FinancialInterest.objects.create(name = row["employer_business_interest"],
-            industry = row["industry"],
-            lawmaker = lawmaker)
-    fi_entity.save()
+    create_fi(row, lawmaker)
+def pop_from_df(df):
+    '''
+    Takes in a dataframe, creates appropriate objects.
+    '''
+    for index, row in df.iterrows():
+       create_lm(index, row)
+
+if __name__ == "__main__":
+    pop_from_df(std_df)
+
+
+
