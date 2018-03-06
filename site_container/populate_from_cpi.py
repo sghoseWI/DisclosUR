b52a107eb863262b58ed78952599064d93f2efe7
Nick Tallant <ndtallant@uchicago.edu>
2018-03-06 05:28:41 -0600

    Can now repopulate all versions of CPI - with clean code, tested and verified in admin

--- site_container/populate_from_cpi.py
+++ site_container/populate_from_cpi.py
@@ -20,40 +20,45 @@ def create_fi(row, lawmaker):
     '''
     Create FI object and associate it to the lawmaker
     '''
-    fi_ob = FinancialInterest(name=row["employer_business_interest"],
-                              industry=row["industry"],
+    fi_ob = FinancialInterest(name=row.employer_business_interest,
+                              industry=row.industry,
                               lawmaker=lawmaker)
     fi_ob.save()
  
 
-def create_lm(index, row):
+def create_lm(index, row, non_standard):
     '''
     Checks to see if LM already exists, creates one if it does not.
+    Has 'non_standard' attributes if nonstandard = True.
     Creates FI object tied to LM either way.
     '''
     if not Lawmaker.objects.filter(pk=index).exists():
-        lawmaker = Lawmaker(name=row["lawmaker"],
-                            state=row["state"],
-                            body=row["body"],
+        lawmaker = Lawmaker(name=row.lawmaker,
+                            state=row.state,
+                            body=row.body,
                             lawmaker_id=index,
-                            district=row["district"],
-                            disclosure_url=row["disclosure_report"],
-                            party=row["party"],
+                            district=row.district,
+                            disclosure_url=row.disclosure_report,
+                            party=row.party,
                             cpi_2015=True)
+        if non_standard:
+            lawmaker.non_standard_FI = row.employer_business_interest
+            lawmaker.non_standard_IF = row.industry
         lawmaker.save()
         print('saving', lawmaker)
 
     lawmaker = Lawmaker.objects.get(pk=index)
     create_fi(row, lawmaker)
-def pop_from_df(df):
+def pop_from_df(df, non_standard):
     '''
     Takes in a dataframe, creates appropriate objects.
     '''
     for index, row in df.iterrows():
-       create_lm(index, row)
+       create_lm(index, row, non_standard)
 
 if __name__ == "__main__":
-    pop_from_df(std_df)
+    pop_from_df(std_df, False)
+    pop_from_df(non_std_df, True)
 
 
 
