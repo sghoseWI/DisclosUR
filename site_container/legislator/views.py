286324f3a14fc2951820b03ab05d6f03550ab92e
Nick Tallant <ndtallant@uchicago.edu>
2018-03-07 19:45:33 -0600

    Bug fix - state search was not rendering appropriately

--- site_container/legislator/views.py
+++ site_container/legislator/views.py
@@ -57,7 +57,7 @@ def state_dist(request, usr_state, usr_dist):
         q_set = Lawmaker.objects.filter(state=usr_state)
     else:
         q_set = Lawmaker.objects.filter(state=usr_state, district = usr_dist)
-    return render(request, 'by_state.html', {"state_table":q_set})
+    return render(request, 'by_state.html', {"lm_table":q_set})
 
 def by_lawmaker(request, lawmaker):
     form = request.GET
